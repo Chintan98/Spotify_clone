50878d3e7867cae9227f17b51ec10d63dd9393ee
Chintan Jain <chintanjain1998@gmail.com>
2023-04-07 01:01:42 +0100

    handle no-js trackList

--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { Player } from '$components';
 	import { msToTime } from '$helpers';
 	import { Clock8, ListPlus } from 'lucide-svelte';
-	import { Player } from '$components';
+	import playingGif from '$assets/playing.gif';
+
+	let currentlyPlaying: string | null = null;
+	let isPaused: boolean = false;
 
 	export let tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[];
 </script>
@@ -21,17 +25,22 @@
 	</div>
 
 	{#each tracks as track, index}
-		<div class="row">
+		<div class="row" class:is-current={currentlyPlaying === track.id}>
 			<div class="number-column">
-				<span class="number">{index + 1}</span>
+				{#if currentlyPlaying === track.id && !isPaused}
+					<img class="playing-gif" src={playingGif} alt="" />
+				{:else}
+					<span class="number">{index + 1}</span>
+				{/if}
 				<div class="player">
 					<Player
 						{track}
 						on:play={(e) => {
-							console.log(e.detail.track);
+							currentlyPlaying = e.detail.track.id;
+							isPaused = false;
 						}}
 						on:pause={(e) => {
-							console.log(e.detail.track);
+							isPaused = e.detail.track.id === currentlyPlaying;
 						}}
 					/>
 				</div>
@@ -67,6 +76,12 @@
 			align-items: center;
 			padding: 7px 5px;
 			border-radius: 4px;
+			&.is-current {
+				.info-column .track-title h4,
+				.number-column span.number {
+					color: var(--accent-color);
+				}
+			}
 			&.header {
 				border-bottom: 1px solid var(--border);
 				border-radius: 0px;
@@ -85,6 +100,15 @@
 			&:not(.header) {
 				&:hover {
 					background-color: rgba(255, 255, 255, 0.05);
+					.number-column {
+						.player {
+							display: block;
+						}
+						span.number,
+						.playing-gif {
+							display: none;
+						}
+					}
 				}
 			}
 			.number-column {
@@ -95,6 +119,12 @@
 				span.number {
 					color: var(--light-gray);
 					font-size: functions.toRem(14);
+				}
+				.playing-gif {
+					width: 12px;
+				}
+				.player {
+					display: none;
 				}
 			}
 			.info-column {
