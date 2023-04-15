03e6748576dc0e8dfb51fb0fcd0aeea9f3943fba
Chintan Jain <chintanjain1998@gmail.com>
2023-04-15 15:15:08 +0100

    remove track from playlist

--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -5,9 +5,13 @@
 	import { Clock8, ListPlus, ListX } from 'lucide-svelte';
 	import playingGif from '$assets/playing.gif';
 	import { page } from '$app/stores';
+	import { enhance } from '$app/forms';
+	import { toasts } from '$stores';
+	import { hideAll } from 'tippy.js';
 
 	let currentlyPlaying: string | null = null;
 	let isPaused: boolean = false;
+	let isAddingToPlaylist: string[] = [];
 
 	export let tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[];
 	export let isOwner: boolean = false;
@@ -95,7 +99,28 @@
 					{#if userPlaylists}
 						<div class="playlists-menu" style="display: none;" id="{track.id}-playlists-menu">
 							<div class="playlist-menu-content">
-								<form method="POST" action="/playlist?/addItem&redirect={$page.url.pathname}">
+								<form
+									method="POST"
+									action="/playlist?/addItem&redirect={$page.url.pathname}"
+									use:enhance={({ cancel }) => {
+										if (isAddingToPlaylist.includes(track.id)) {
+											cancel();
+										}
+										isAddingToPlaylist = [...isAddingToPlaylist, track.id];
+										return ({ result }) => {
+											if (result.type === 'error') toasts.error(result.error.message);
+											if (result.type === 'redirect') {
+												const url = new URL(`${$page.url.origin}${result.location}`);
+												const error = url.searchParams.get('error');
+												const success = url.searchParams.get('success');
+												if (error) toasts.error(error);
+												if (success) toasts.success(success);
+												hideAll();
+											}
+											isAddingToPlaylist = isAddingToPlaylist.filter((t) => t !== track.id);
+										};
+									}}
+								>
 									<input hidden value={track.id} name="track" />
 									<div class="field">
 										<select name="playlist" aria-label="Playlist">
@@ -105,7 +130,11 @@
 										</select>
 									</div>
 									<div class="submit-button">
-										<Button element="button" type="submit">
+										<Button
+											disabled={isAddingToPlaylist.includes(track.id)}
+											element="button"
+											type="submit"
+										>
 											Add <span class="visually-hidden">{track.name} to selected playlist.</span>
 										</Button>
 									</div>
